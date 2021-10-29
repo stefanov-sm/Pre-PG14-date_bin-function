fa19972208ae9fbd43db6fc8d80d669ab25d270b
Stefan Stefanov <stefanov.sm@abv.bg>
2021-10-29 20:35:18 +0300

    Add files via upload

---
+++ date_bin.sql
@@ -0,0 +1,9 @@
+create or replace function date_bin
+ (trunc_period interval, ts timestamptz, base_ts timestamptz default '1970-01-01Z')
+ returns timestamptz language sql immutable as
+$$
+select
+  base_ts
+  + floor(extract(epoch from ts - base_ts) / extract(epoch from trunc_period))::bigint
+  * trunc_period;
+$$;
