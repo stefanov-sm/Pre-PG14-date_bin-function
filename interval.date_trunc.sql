93ddc85632f86bfc18c5849976be126b138e6133
Stefan Stefanov <stefanov.sm@abv.bg>
2021-10-29 17:26:10 +0300

    Delete interval.date_trunc.sql

--- interval.date_trunc.sql
+++
@@ -1,9 +0,0 @@
-create or replace function date_bin
-  (trunc_period interval, ts timestamptz, base_ts timestamptz default '1970-01-01Z')
-  returns timestamptz language sql immutable as
-$function$
-select
-  base_ts
-  + floor(extract(epoch from ts - base_ts) / extract(epoch from trunc_period))::bigint
-  * trunc_period;
-$function$;
